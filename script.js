8f24b26eac5ebf9d32bdf13a8ce7ad1a685758b1
HugoLaurent <lauhugopro@gmail.com>
2023-02-20 19:30:52 +0100

    resolution du timing entre le titre et le paragraphe

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const mainTitle = document.querySelector("main h1")
 const splitText = mainTitle.innerText.split('');
+const animationParagraphe = document.querySelector("article p")
+const endOfFunction = new Event("endEvent");
 
 mainTitle.innerHTML = '';
 i = 0
-setInterval(function(){
-    AjoutDeLettre();
-    setTimeout(function(){
-        pAppear()
-    }, 3000)
+
+titleAppear()
+
+function titleAppear() {
+    setInterval(function(){
+        AjoutDeLettre();
+       
+        // setTimeout(function(){
+        //     pAppear()
+        // }, 3000)
+        
+    }
+    , 100 )
+    
 }
-  , 100 )
+
+document.addEventListener("endEvent", () => {
+    pAppear();
+})
+
 
 function AjoutDeLettre(){
     if(i < splitText.length){
         mainTitle.innerHTML += splitText[i];
     i++;
+    } else {
+        document.dispatchEvent(endOfFunction)
     }
 }
-
-const animationParagraphe = document.querySelector("article p")
 
 function pAppear() {
     animationParagraphe.classList.add("after-animation")
